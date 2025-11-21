34d105d5bb59d51364ddb1ac7ce514738a8a3103
Luan Alves da Silva <luanalvesdasilva278@gmail.com>
2025-11-20 22:28:30 -0300

    correção das coisa

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -11,10 +11,10 @@ const App: React.FC = () => {
     <Router>
       <div>
         <Header />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/resume" component={Resume} />
-        </Switch>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/resume" element={<Resume />} />
+        </Routes>
         <Footer />
       </div>
     </Router>
