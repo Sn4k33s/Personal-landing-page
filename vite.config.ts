1c00848b379ed11b9e5809d0c4be985b4f9713d7
Luan Alves da Silva <133814201+Sn4k33s@users.noreply.github.com>
2025-11-20 22:02:36 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Personal-landing-page/',
   plugins: [react()],
   server: {
     open: true,
